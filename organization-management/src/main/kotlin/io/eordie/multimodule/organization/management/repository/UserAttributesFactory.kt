b697b77250b6409d8f24d3ac9dabf4fe1a5efc23
a8t3r <winter.schweigen@gmail.com>
2024-07-22 14:22:40 +0300

    wip

--- organization-management/src/main/kotlin/io/eordie/multimodule/organization/management/repository/UserAttributesFactory.kt
+++ organization-management/src/main/kotlin/io/eordie/multimodule/organization/management/repository/UserAttributesFactory.kt
@@ -1,12 +1,13 @@
 package io.eordie.multimodule.organization.management.repository
 
 import io.eordie.multimodule.common.repository.KFactoryImpl
+import io.eordie.multimodule.organization.management.models.UserAttributeModel
 import jakarta.inject.Singleton
 import java.util.*
 
 @Singleton
-class UserAttributesFactory : KFactoryImpl<io.eordie.multimodule.organization.management.models.UserAttributeModel, UUID>(
-    io.eordie.multimodule.organization.management.models.UserAttributeModel::class
+class UserAttributesFactory : KFactoryImpl<UserAttributeModel, UUID>(
+    UserAttributeModel::class
 ) {
 
     override val datasourceName = "keycloak"
