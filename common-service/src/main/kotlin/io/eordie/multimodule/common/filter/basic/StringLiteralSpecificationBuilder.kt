a2bf7173aec7e63b8c20b39ea897f9f247b0759d
a8t3r <winter.schweigen@gmail.com>
2024-07-26 16:04:36 +0300

    empty regexp

--- common-service/src/main/kotlin/io/eordie/multimodule/common/filter/basic/StringLiteralSpecificationBuilder.kt
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/filter/basic/StringLiteralSpecificationBuilder.kt
@@ -4,7 +4,7 @@ import io.eordie.multimodule.contracts.basic.filters.StringLiteralFilter
 import org.babyfish.jimmer.sql.ast.LikeMode
 import org.babyfish.jimmer.sql.kt.ast.expression.KExpression
 import org.babyfish.jimmer.sql.kt.ast.expression.KNonNullExpression
-import org.babyfish.jimmer.sql.kt.ast.expression.ilike
+import org.babyfish.jimmer.sql.kt.ast.expression.`ilike?`
 import org.babyfish.jimmer.sql.kt.ast.expression.not
 
 class StringLiteralSpecificationBuilder : ComparableLiteralSpecificationBuilder<StringLiteralFilter, String>() {
@@ -14,10 +14,10 @@ class StringLiteralSpecificationBuilder : ComparableLiteralSpecificationBuilder<
         path: KExpression<String>
     ): List<KNonNullExpression<Boolean>> {
         val secondary = listOfNotNull(
-            filter.like?.let { path.ilike(it, LikeMode.ANYWHERE) },
-            filter.nlike?.let { path.ilike(it, LikeMode.ANYWHERE).not() },
-            filter.startsWith?.let { path.ilike(it, LikeMode.START) },
-            filter.endsWith?.let { path.ilike(it, LikeMode.END) }
+            filter.like?.let { path.`ilike?`(it, LikeMode.ANYWHERE) },
+            filter.nlike?.let { path.`ilike?`(it, LikeMode.ANYWHERE)?.not() },
+            filter.startsWith?.let { path.`ilike?`(it, LikeMode.START) },
+            filter.endsWith?.let { path.`ilike?`(it, LikeMode.END) }
         )
 
         return super.invoke(filter, path) + secondary
