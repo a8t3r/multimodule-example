22605e87e542880031cbad3ef83a59cb971537bf
a8t3r <winter.schweigen@gmail.com>
2024-07-30 00:48:12 +0300

    GenericTypes

--- common-service/src/main/kotlin/io/eordie/multimodule/common/utils/GenericTypes.kt
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/utils/GenericTypes.kt
@@ -6,13 +6,10 @@ import kotlin.reflect.jvm.jvmErasure
 
 object GenericTypes {
 
-    private val cache = mutableMapOf<Pair<KClass<*>, KClass<*>>, KClass<*>>()
+    private val cache = mutableMapOf<Pair<KClass<*>, KClass<*>>, List<KClass<*>>>()
 
-    // no cache support
     fun <T : Any> getTypeArguments(instance: Any, targetType: KClass<T>): List<KClass<*>> {
-        return instance::class.allSupertypes
-            .single { it.classifier == targetType }
-            .arguments.map { requireNotNull(it.type?.jvmErasure) }
+        return getTypeArgumentsFromClass(instance::class, targetType)
     }
 
     fun <T : Any> getTypeArgument(instance: T, targetType: KClass<T>): KClass<*> {
@@ -20,12 +17,14 @@ object GenericTypes {
     }
 
     fun <T : Any> getTypeArgumentFromClass(instanceType: KClass<out T>, targetType: KClass<T>): KClass<*> {
-        return cache.getOrPut(instanceType to targetType) {
-            val result = instanceType.allSupertypes
-                .single { it.classifier == targetType }
-                .arguments.single().type?.jvmErasure
+        return getTypeArgumentsFromClass(instanceType, targetType).single()
+    }
 
-            requireNotNull(result)
+    private fun getTypeArgumentsFromClass(instanceType: KClass<*>, targetType: KClass<*>): List<KClass<*>> {
+        return cache.getOrPut(instanceType to targetType) {
+            instanceType.allSupertypes
+                .single { it.classifier == targetType }
+                .arguments.map { requireNotNull(it.type?.jvmErasure) }
         }
     }
 }
