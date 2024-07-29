929c85f99c8134721bcce5bfdd0f08aa28816643
a8t3r <winter.schweigen@gmail.com>
2024-07-30 00:02:31 +0300

    failOnUnknown

--- common-service/src/main/kotlin/io/eordie/multimodule/common/utils/FetcherBuilder.kt
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/utils/FetcherBuilder.kt
@@ -8,16 +8,19 @@ import org.babyfish.jimmer.sql.fetcher.impl.FetcherImplementor
 
 class FetcherBuilder<T>(private val type: Class<T>) {
 
-    fun newFetcher(fields: List<String>): Fetcher<T> {
+    fun newFetcher(fields: List<String>, failOnUnknown: Boolean): Fetcher<T> {
         val propertiesMap = ImmutableType.get(type).selectableScalarProps
         return fields.fold(FetcherImpl(type) as FetcherImplementor<T>) { acc, field ->
             if (field.contains('.')) acc else {
                 val property = propertiesMap[field]?.takeIf { it.isScalar(TargetLevel.OBJECT) }
                     ?: propertiesMap[field + "Id"]
                     ?: propertiesMap[field.dropLast(1) + "Ids"]
-                    ?: error("unknown property [$field] in projection")
 
-                acc.add(property.name)
+                when {
+                    property != null -> acc.add(property.name)
+                    failOnUnknown -> error("unknown property [$field] in projection")
+                    else -> acc
+                }
             }
         }
     }
--- common-service/src/main/kotlin/io/eordie/multimodule/common/utils/SelectionSetExt.kt
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/utils/SelectionSetExt.kt
@@ -4,17 +4,18 @@ import io.eordie.multimodule.contracts.basic.paging.Page
 import io.eordie.multimodule.contracts.basic.paging.SelectionSet
 import org.babyfish.jimmer.sql.fetcher.Fetcher
 
-inline fun <reified T : Any> SelectionSet.asPageFetcher(): Fetcher<T> {
+inline fun <reified T : Any> SelectionSet.asPageFetcher(failOnUnknown: Boolean = true): Fetcher<T> {
     val entityFields = fields
         .filter { it.startsWith(Page<*>::data.name) }
         .map { it.drop(5) }
         .filter { it.isNotEmpty() }
 
-    return fetcherImpl(entityFields, T::class.java)
+    return fetcherImpl(entityFields, T::class.java, failOnUnknown)
 }
 
-inline fun <reified T : Any> SelectionSet.asFetcher(): Fetcher<T> {
-    return fetcherImpl(fields, T::class.java)
+inline fun <reified T : Any> SelectionSet.asFetcher(failOnUnknown: Boolean = true): Fetcher<T> {
+    return fetcherImpl(fields, T::class.java, failOnUnknown)
 }
 
-fun <T : Any> fetcherImpl(fields: List<String>, type: Class<T>): Fetcher<T> = FetcherBuilder(type).newFetcher(fields)
+fun <T : Any> fetcherImpl(fields: List<String>, type: Class<T>, failOnUnknown: Boolean): Fetcher<T> =
+    FetcherBuilder(type).newFetcher(fields, failOnUnknown)
