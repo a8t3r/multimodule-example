3bcdadc3a0076a3075784e8c1a0721ca8c44b6a9
a8t3r <winter.schweigen@gmail.com>
2024-08-05 12:22:40 +0300

    remove keycloak

--- organization-management/build.gradle.kts
+++ organization-management/build.gradle.kts
@@ -5,10 +5,6 @@ plugins {
 dependencies {
     ksp("io.micronaut.data:micronaut-data-processor")
 
-    implementation("io.phasetwo:phasetwo-admin-client:0.1.9") {
-        exclude(group = "com.fasterxml.jackson.module", module = "jackson-module-jaxb-annotations")
-    }
-
     testImplementation("org.testcontainers:testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
