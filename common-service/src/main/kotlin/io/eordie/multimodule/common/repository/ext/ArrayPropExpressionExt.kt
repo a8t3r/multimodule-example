2f2881395dc585d7604098f31ef8f610fa2bb8f1
a8t3r <winter.schweigen@gmail.com>
2024-08-10 19:18:00 +0300

    split files

---
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/repository/ext/ArrayPropExpressionExt.kt
@@ -0,0 +1,74 @@
+package io.eordie.multimodule.common.repository.ext
+
+import org.babyfish.jimmer.sql.kt.ast.expression.KExpression
+import org.babyfish.jimmer.sql.kt.ast.expression.KNonNullExpression
+import org.babyfish.jimmer.sql.kt.ast.expression.and
+import org.babyfish.jimmer.sql.kt.ast.expression.or
+import org.babyfish.jimmer.sql.kt.ast.expression.sql
+
+private fun <T : Any> KExpression<List<T>>.operator(value: T, name: String): KNonNullExpression<Boolean> {
+    val expression = this
+    return sql(Boolean::class, "%v $name ANY(%e)") {
+        value(value)
+        expression(expression)
+    }
+}
+
+fun <T : Any> KExpression<List<T>>.contains(value: T) = operator(value, "=")
+fun <T : Any> KExpression<List<T>>.notContains(value: T) = operator(value, "!=")
+fun <T : Any> KExpression<List<T>>.gt(value: T) = operator(value, "<")
+fun <T : Any> KExpression<List<T>>.ge(value: T) = operator(value, "<=")
+fun <T : Any> KExpression<List<T>>.lt(value: T) = operator(value, ">")
+fun <T : Any> KExpression<List<T>>.le(value: T) = operator(value, ">=")
+
+inline fun <reified V : Any> KExpression<*>.cast(): KNonNullExpression<V> {
+    val expression = this
+    return sql(V::class, "cast(%e as ${V::class.simpleName})") {
+        expression(expression)
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Any> KExpression<List<T>>.intersection(values: Array<T>): KNonNullExpression<List<T>> {
+    val expression = this
+    return sql(List::class, "array_intersect(%e, array[%v])") {
+        value(values)
+        expression(expression)
+    } as KNonNullExpression<List<T>>
+}
+
+fun <T : Any> KExpression<List<T>>.overlap(values: Array<T>): KNonNullExpression<Boolean> {
+    val expression = this
+    return sql(Boolean::class, "%e && array[%v]") {
+        value(values)
+        expression(expression)
+    }
+}
+
+val <T : Any> KExpression<List<T>>.arraySize: KNonNullExpression<Int> get() = run {
+    val expression = this
+    return sql(Int::class, "array_length(%e, 1)") {
+        expression(expression)
+    }
+}
+
+fun <T : Any> KExpression<List<T>>.arrayLike(pattern: String): KNonNullExpression<Boolean> {
+    val expression = this
+    return sql(Boolean::class, "(array_to_string(%e, ',') ilike %v or array_to_string(%e, ',') ilike ',' || %v)") {
+        expression(expression)
+        value(pattern)
+    }
+}
+
+fun List<KNonNullExpression<Boolean>?>?.or(): KNonNullExpression<Boolean>? =
+    if (this.isNullOrEmpty()) null else or(*this.toTypedArray())
+
+fun List<KNonNullExpression<Boolean>?>?.and(): KNonNullExpression<Boolean>? =
+    if (this.isNullOrEmpty()) null else and(*this.toTypedArray())
+
+fun <T : Any> KExpression<T>.arrayAgg(distinct: Boolean = true): KNonNullExpression<out List<T>> {
+    val property = this
+    return sql(emptyList<T>()::class, "array_agg(${if (distinct) "distinct" else ""} %e)") {
+        expression(property)
+    }
+}
--- common-service/src/main/kotlin/io/eordie/multimodule/common/repository/ext/BasePropExpressionExt.kt
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/repository/ext/BasePropExpressionExt.kt
@@ -4,10 +4,7 @@ import org.babyfish.jimmer.sql.ast.impl.table.TableImplementor
 import org.babyfish.jimmer.sql.ast.table.spi.PropExpressionImplementor
 import org.babyfish.jimmer.sql.kt.ast.expression.KExpression
 import org.babyfish.jimmer.sql.kt.ast.expression.KNonNullExpression
-import org.babyfish.jimmer.sql.kt.ast.expression.and
 import org.babyfish.jimmer.sql.kt.ast.expression.not
-import org.babyfish.jimmer.sql.kt.ast.expression.or
-import org.babyfish.jimmer.sql.kt.ast.expression.sql
 
 fun <T : Any> KExpression<T>.name(): String {
     val propImplementor = this as PropExpressionImplementor<*>
@@ -20,71 +17,4 @@ fun <T : Any> KExpression<T>.name(): String {
 
 fun KNonNullExpression<Boolean>?.negateUnless(condition: Boolean?): KNonNullExpression<Boolean>? {
     return if (this == null || condition != false) this else this.not()
-}
-
-private fun <T : Any> KExpression<List<T>>.operator(value: T, name: String): KNonNullExpression<Boolean> {
-    val expression = this
-    return sql(Boolean::class, "%v $name ANY(%e)") {
-        value(value)
-        expression(expression)
-    }
-}
-
-fun <T : Any> KExpression<List<T>>.contains(value: T) = operator(value, "=")
-fun <T : Any> KExpression<List<T>>.notContains(value: T) = operator(value, "!=")
-fun <T : Any> KExpression<List<T>>.gt(value: T) = operator(value, "<")
-fun <T : Any> KExpression<List<T>>.ge(value: T) = operator(value, "<=")
-fun <T : Any> KExpression<List<T>>.lt(value: T) = operator(value, ">")
-fun <T : Any> KExpression<List<T>>.le(value: T) = operator(value, ">=")
-
-inline fun <reified V : Any> KExpression<*>.cast(): KNonNullExpression<V> {
-    val expression = this
-    return sql(V::class, "cast(%e as ${V::class.simpleName})") {
-        expression(expression)
-    }
-}
-
-@Suppress("UNCHECKED_CAST")
-fun <T : Any> KExpression<List<T>>.intersection(values: Array<T>): KNonNullExpression<List<T>> {
-    val expression = this
-    return sql(List::class, "array_intersect(%e, array[%v])") {
-        value(values)
-        expression(expression)
-    } as KNonNullExpression<List<T>>
-}
-
-fun <T : Any> KExpression<List<T>>.overlap(values: Array<T>): KNonNullExpression<Boolean> {
-    val expression = this
-    return sql(Boolean::class, "%e && array[%v]") {
-        value(values)
-        expression(expression)
-    }
-}
-
-val <T : Any> KExpression<List<T>>.arraySize: KNonNullExpression<Int> get() = run {
-    val expression = this
-    return sql(Int::class, "array_length(%e, 1)") {
-        expression(expression)
-    }
-}
-
-fun <T : Any> KExpression<List<T>>.arrayLike(pattern: String): KNonNullExpression<Boolean> {
-    val expression = this
-    return sql(Boolean::class, "(array_to_string(%e, ',') ilike %v or array_to_string(%e, ',') ilike ',' || %v)") {
-        expression(expression)
-        value(pattern)
-    }
-}
-
-fun List<KNonNullExpression<Boolean>?>?.or(): KNonNullExpression<Boolean>? =
-    if (this.isNullOrEmpty()) null else or(*this.toTypedArray())
-
-fun List<KNonNullExpression<Boolean>?>?.and(): KNonNullExpression<Boolean>? =
-    if (this.isNullOrEmpty()) null else and(*this.toTypedArray())
-
-fun <T : Any> KExpression<T>.arrayAgg(distinct: Boolean = true): KNonNullExpression<out List<T>> {
-    val property = this
-    return sql(emptyList<T>()::class, "array_agg(${if (distinct) "distinct" else ""} %e)") {
-        expression(property)
-    }
 }
