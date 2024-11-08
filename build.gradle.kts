5993a0cb06de1334493f6e1d3fb4242dc938feb5
a8t3r <winter.schweigen@gmail.com>
2024-11-08 12:26:58 +0300

    micronaut version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
     id("nl.littlerobots.version-catalog-update") version "0.8.5"
-    id("com.autonomousapps.dependency-analysis") version "2.4.2"
 }
 
 idea {
