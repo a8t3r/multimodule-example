719b615adba96fc6075f4702fa9dd89fd2f91a0a
a8t3r <winter.schweigen@gmail.com>
2024-11-08 12:11:49 +0300

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
     id("nl.littlerobots.version-catalog-update") version "0.8.5"
+    id("com.autonomousapps.dependency-analysis") version "2.4.2"
 }
 
 idea {
