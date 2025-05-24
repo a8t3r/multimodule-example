7e1edc8b41a156071c9d81814a851c041a154f9e
a8t3r <winter.schweigen@gmail.com>
2025-05-24 19:17:46 +0300

    kobby returns

--- api-tests/src/test/kotlin/io/eordie/multimodule/api/tests/library/LibraryTest.kt
+++ api-tests/src/test/kotlin/io/eordie/multimodule/api/tests/library/LibraryTest.kt
@@ -246,4 +246,25 @@ class LibraryTest : AbstractApplicationTest() {
         assertThat(firstBook.name).isEqualTo("Third book")
         assertThat(firstBook.authorIds).hasSize(1)
     }
+
+    @Test
+    fun `should retrieve authors from book by graphql query`() = test {
+        val result = schema.query {
+            books {
+                data {
+                    id()
+                    name()
+                    authors {
+                        id()
+                        fullName()
+                    }
+                }
+            }
+        }
+
+        assertThat(result.books.data).isNotNull().hasSize(2)
+        result.books.data?.forEach { book ->
+            assertThat(book?.authors).isNotNull().hasSize(1)
+        }
+    }
 }
