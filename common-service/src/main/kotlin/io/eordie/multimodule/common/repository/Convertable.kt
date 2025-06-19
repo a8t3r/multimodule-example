d74874451440ed4fe55fceb59ddf0e5fb7d90762
a8t3r <winter.schweigen@gmail.com>
2025-06-19 16:14:24 +0300

    wip

--- common-service/src/main/kotlin/io/eordie/multimodule/common/repository/Convertable.kt
+++ common-service/src/main/kotlin/io/eordie/multimodule/common/repository/Convertable.kt
@@ -2,5 +2,4 @@ package io.eordie.multimodule.common.repository
 
 interface Convertable<T : Any> {
     fun convert(): T = EntityConverter.convert(this)
-    fun convert(block: (T) -> Unit) = convert().apply(block)
 }
