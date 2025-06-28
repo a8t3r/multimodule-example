f227eae458732e6685f38990988f0f6ac4363e64
a8t3r <winter.schweigen@gmail.com>
2025-06-28 10:53:45 +0300

    wip

--- model-contracts/src/main/kotlin/io/eordie/multimodule/contracts/basic/RequestHeaders.kt
+++ model-contracts/src/main/kotlin/io/eordie/multimodule/contracts/basic/RequestHeaders.kt
@@ -2,6 +2,7 @@ package io.eordie.multimodule.contracts.basic
 
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.core.convert.ArgumentConversionContext
+import io.micronaut.core.convert.ConversionService
 import io.micronaut.core.type.Headers
 import kotlinx.serialization.Serializable
 import java.util.*
@@ -13,6 +14,10 @@ data class RequestHeaders(private val data: Map<String, List<String>>) : Headers
     override fun get(name: CharSequence): String? = data[name]?.firstOrNull()
     override fun names(): Set<String> = data.keys
     override fun values(): Collection<List<String>> = data.values
-    override fun <T : Any?> get(name: CharSequence, conversionContext: ArgumentConversionContext<T>): Optional<T> =
-        throw UnsupportedOperationException()
+    override fun <T : Any> get(name: CharSequence, conversionContext: ArgumentConversionContext<T>): Optional<T> {
+        val value = get(name)
+        return if (value == null) Optional.empty<T>() else {
+            ConversionService.SHARED.convert(value, conversionContext)
+        }
+    }
 }
